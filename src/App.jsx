88ecf02a4de9709b2d63a0e362c8097eef1dd5ac
Jordyn Sonne <jordynsonne@Jordyns-MacBook-Air.local>
2024-05-20 19:27:19 -0500

    done

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { Route, Routes, Link} from 'react-router-dom'
+import Blue from './Components/Blue'
+import Red from './Components/Red'
+import Home from './Components/Home'
+
+function App() {
+  
+return(
+  <>
+  <div id="container">
+      <h1>Hello React Router!</h1>
+      <div id="navbar">
+        <Link to='/blue'>Blue</Link>
+        <Link to='/red'>Red</Link>
+        <Link to='/'>Home</Link>
+      </div>
+      <div id="main-section">
+        <Routes>
+          <Route path='/blue' element={<Blue/>}/>
+          <Route path='/red' element={<Red/>}/>
+          <Route path='/' element={<Home/>}/>
+          
+        </Routes></div>
+    </div>
+</>
+)
+  
+   
+}
+
+export default App
---
+++ src/Components/Blue.jsx
@@ -0,0 +1,14 @@
+
+
+function Blue(){
+    return(
+        <>
+        <div className="blue">
+            <h1>Blue</h1>
+        </div>
+        
+        </>
+    )
+}
+
+export default Blue
